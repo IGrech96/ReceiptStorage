98dfc8811b9bb86e93df25abee7ae73464cb5103
Ivan Grechko <ivang2896@gmail.com>
2024-05-25 18:57:17 +0300

    Update tests

--- ReceiptStorage.Tests/TemplateTests.cs
+++ ReceiptStorage.Tests/TemplateTests.cs
@@ -8,15 +8,15 @@ namespace ReceiptStorage.Tests
         [Fact]
         public async Task MtbTest()
         {
-            var stream = new FileStream("Source\\mtb_payment.pdf", FileMode.Open);
+            var stream = new FileStream("Source\\test_payment.pdf", FileMode.Open);
             var template = new MtbankTemplate();
             var result = await template.TryExtractAsync(new PdfDocument(new PdfReader(stream)), CancellationToken.None);
 
-            Assert.Equal("Коммунальные платежи", result.Title);
+            Assert.Equal("Пополнение счета", result.Title);
             Assert.Equal("MTB", result.Type);
             Assert.Equal("BYN", result.Currency);
-            Assert.Equal(new DateTime(2024,5,18, 17,29,1), result.Timestamp);
-            Assert.Equal(212.73, result.Amount, 7);
+            Assert.Equal(new DateTime(2024,5,24, 19,56,13), result.Timestamp);
+            Assert.Equal(35.0, result.Amount, 7);
         }
     }
 }
